541cbb6b17c6a59b7f760a79babb1c73caf60c03
Mahesh <kmaheshgupta151@outlook.com>
2025-10-15 22:32:54 +0530

    remove key

--- app/services/firecrawl.py
+++ app/services/firecrawl.py
@@ -41,7 +41,7 @@ class FirecrawlService:
         print('before gemini')
 
         # ensure configured earlier or do it here:
-        client = genai.Client(api_key='')
+        client = genai.Client(api_key=' ')
         print('after making client')
 
         prompt = prompts.extract_job_v2(markdown_content)
